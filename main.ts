48348cae085aa826f9e7e98f04de26800c99841c
Lon Landry <lonlandry@hotmail.com>
2023-01-25 15:28:18 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,436 @@
+basic.showString("Ready for HIIT!")
+basic.showLeds(`
+    # . # # #
+    # . # . #
+    # . # . #
+    # . # . #
+    # . # # #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # # # # .
+    # . . # .
+    # # # # .
+    . . . # .
+    . . . # .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    . # # # .
+    . # . # .
+    . # # # .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # # # # .
+    . . . # .
+    . . . # .
+    . . . # .
+    . . . # .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # # # # .
+    # . . # .
+    # # # # .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # # # # .
+    # . . . .
+    # # # # .
+    . . . # .
+    # # # # .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # . . # .
+    # . . # .
+    # # # # .
+    . . . # .
+    . . . # .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # # # # .
+    . . . # .
+    # # # # .
+    . . . # .
+    # # # # .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # # # # .
+    . . . # .
+    # # # # .
+    # . . . .
+    # # # # .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . # . .
+    . # # . .
+    . . # . .
+    . . # . .
+    . # # # .
+    `)
+basic.pause(1000)
+for (let index = 0; index < 10; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(15000)
+    basic.showLeds(`
+        # # # # .
+        # . . . .
+        # # # # .
+        . . . # .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . . # .
+        # . . # .
+        # # # # .
+        . . . # .
+        . . . # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # .
+        . . . # .
+        # # # # .
+        . . . # .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # .
+        . . . # .
+        # # # # .
+        # . . . .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showIcon(IconNames.StickFigure)
+    basic.pause(5000)
+    basic.showLeds(`
+        # # # # .
+        # . . . .
+        # # # # .
+        . . . # .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . . # .
+        # . . # .
+        # # # # .
+        . . . # .
+        . . . # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # .
+        . . . # .
+        # # # # .
+        . . . # .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # .
+        . . . # .
+        # # # # .
+        # . . . .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(1000)
+}
+basic.showString("5 Min.")
+basic.pause(20000)
+for (let index = 0; index < 10; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(15000)
+    basic.showLeds(`
+        # # # # .
+        # . . . .
+        # # # # .
+        . . . # .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . . # .
+        # . . # .
+        # # # # .
+        . . . # .
+        . . . # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # .
+        . . . # .
+        # # # # .
+        . . . # .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # .
+        . . . # .
+        # # # # .
+        # . . . .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showIcon(IconNames.StickFigure)
+    basic.pause(5000)
+    basic.showLeds(`
+        # # # # .
+        # . . . .
+        # # # # .
+        . . . # .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . . # .
+        # . . # .
+        # # # # .
+        . . . # .
+        . . . # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # .
+        . . . # .
+        # # # # .
+        . . . # .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # .
+        . . . # .
+        # # # # .
+        # . . . .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(1000)
+}
+basic.showString("10 Min.")
+basic.pause(20000)
+for (let index = 0; index < 10; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(15000)
+    basic.showLeds(`
+        # # # # .
+        # . . . .
+        # # # # .
+        . . . # .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . . # .
+        # . . # .
+        # # # # .
+        . . . # .
+        . . . # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # .
+        . . . # .
+        # # # # .
+        . . . # .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # .
+        . . . # .
+        # # # # .
+        # . . . .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showIcon(IconNames.StickFigure)
+    basic.pause(5000)
+    basic.showLeds(`
+        # # # # .
+        # . . . .
+        # # # # .
+        . . . # .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . . # .
+        # . . # .
+        # # # # .
+        . . . # .
+        . . . # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # .
+        . . . # .
+        # # # # .
+        . . . # .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # .
+        . . . # .
+        # # # # .
+        # . . . .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(1000)
+}
+basic.showString("15 Min.")
+basic.pause(20000)
+for (let index = 0; index < 10; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(15000)
+    basic.showLeds(`
+        # # # # .
+        # . . . .
+        # # # # .
+        . . . # .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . . # .
+        # . . # .
+        # # # # .
+        . . . # .
+        . . . # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # .
+        . . . # .
+        # # # # .
+        . . . # .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # .
+        . . . # .
+        # # # # .
+        # . . . .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showIcon(IconNames.StickFigure)
+    basic.pause(5000)
+    basic.showLeds(`
+        # # # # .
+        # . . . .
+        # # # # .
+        . . . # .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . . # .
+        # . . # .
+        # # # # .
+        . . . # .
+        . . . # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # .
+        . . . # .
+        # # # # .
+        . . . # .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # .
+        . . . # .
+        # # # # .
+        # . . . .
+        # # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(1000)
+}
+basic.showIcon(IconNames.Chessboard)
+basic.pause(2000)
+basic.showString("20 Min!")
+basic.pause(5000)
+basic.showIcon(IconNames.Chessboard)
